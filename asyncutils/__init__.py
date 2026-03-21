ccae9311ce149b08a16323e228ca1e4a45241a17
Jonathan Dung <jonathandung@yahoo.com>
2026-03-21 19:45:33 +0800

    fix linter error

--- asyncutils/__init__.py
+++ asyncutils/__init__.py
@@ -1,7 +1,7 @@
 from .version import VersionInfo as V
 __hexversion__, preloaded_submodules = int(__version__ := V(0, 8, 14)), frozenset(('config', 'exceptions', 'version'))
 def __getattr__(name, /, g=globals()):
-    from ._internal import initialize as I; g.update(__getattr__=I.module, __all__=I.a, __dir__=lambda: __all__, submodules_map=I.s); del I # type: ignore
+    from ._internal import initialize as I; g.update(__getattr__=I.module, __all__=(a := I.a), __dir__=lambda: a, submodules_map=I.s); del I
     try: return g[name]
     except KeyError: return __getattr__(name)
 del V
