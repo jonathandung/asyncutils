9a2ed3a0665770fee77254b2ac379be24674572f
Jonathan Dung <jonathandung@yahoo.com>
2026-03-26 15:40:39 +0800

    Fix unbound globals case

--- asyncutils/__main__.py
+++ asyncutils/__main__.py
@@ -2,8 +2,8 @@ if __name__ != '__main__': raise ImportError('cannot import asyncutils.__main__'
 import sys as S
 S.path.insert(0, S.path[0][:-11]); a, b = map(S.intern, ('_frozen_importlib', 'ModuleSpec'))
 if s := globals().get('__spec__'):
-    for g in type(s).__mro__:
-        if g.__module__ == a and g.__qualname__ == b == g.__name__: s.name = 'asyncutils.__main__'; break
-del a, b, g, s, S
+    for S in type(s).__mro__:
+        if S.__module__ == a and S.__qualname__ == b == S.__name__: s.name = 'asyncutils.__main__'; break
+del a, b, s, S
 from .cli import run
 raise SystemExit(run())
