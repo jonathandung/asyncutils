4e17a726ad71c4816e817adc9d6b8aed55f77993
Jonathan Dung <jonathandung@yahoo.com>
2026-03-26 13:39:40 +0800

    cherry-picked

--- tests/test_queues.py
+++ tests/test_queues.py
@@ -1,5 +1,24 @@
+from pytest import raises, fixture, mark
+from asyncutils.config import _randinst
 from asyncutils.queues import *
-def test_pwdq():
-    Q = password_queue('password')
-    Q.put_nowait(0, 'password')
-    assert Q.get_nowait() == 0
+from asyncio.queues import QueueEmpty, QueueFull
+from asyncio.tasks import create_task, sleep
+@fixture
+def pwd(): return _randinst.randbytes(8)
+@mark.asyncio
+async def test_pwdq(pwd):
+    Q = password_queue(pwd, maxsize=2, init_items=[0, 1])
+    await sleep(0.1)
+    assert Q.full()
+    with raises(QueueFull): Q.put_nowait(-1, pwd)
+    t = create_task(Q.put(2, pwd))
+    assert 0 == await Q.get()
+    assert 1 == Q.get_nowait()
+    await sleep(0.1)
+    assert t.done() and 2 == await Q.get()
+    t = create_task(Q.get())
+    Q.put_nowait(3, pwd)
+    await sleep(0.1)
+    assert t.done() and Q.empty()
+    with raises(QueueEmpty): Q.get_nowait()
+    Q.shutdown()
