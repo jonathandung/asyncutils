53348a69afcce82658b2ab9c76cc42f4c37f9913
Jonathan Dung <jonathandung@yahoo.com>
2026-03-28 21:27:06 +0800

    fix test

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,4 +1,4 @@
-import asyncio, os
+import asyncio, os, sys
 from asyncutils.signals import wait_for_signal
 from asyncutils.base import event_loop
 from signal import Signals
@@ -9,7 +9,7 @@ async def kill(sig):
 async def processor(sig): return sig.value
 async def bad_processor(sig): return 1/0
 @mark.asyncio
-@mark.skipif(W := __import__('sys').platform == 'win32', reason='difficult to test signal handling on windows')
+@mark.skipif(W := sys.platform == 'win32', reason='difficult to test signal handling on windows')
 @mark.parametrize('res', range(1, 9))
 async def test_signal(res):
     asyncio.create_task(kill(sig := Signals(res)))
@@ -18,7 +18,7 @@ class Log(BaseException): ...
 def raise_(msg, exc_info=False): raise Log(msg)
 def ignore(*_): ...
 @fixture(scope='module')
-def mock_logger(): return type(__import__('sys').implementation)(warning=raise_, error=raise_, info=ignore, debug=ignore)
+def mock_logger(): return type(sys.implementation)(warning=raise_, error=raise_, info=ignore, debug=ignore) # type: ignore
 @fixture(scope='module')
 def wait_partial(mock_logger): return __import__('_functools').partial(wait_for_signal, processor, logger=mock_logger)
 @fixture
@@ -27,7 +27,7 @@ def run_in_loop():
 def test_signal_log(wait_partial, run_in_loop):
     with raises(Log, match='invalid signal None: .*'): run_in_loop(wait_partial(None))
     with raises(Log, match='wait_for_signal timed out'): run_in_loop(wait_partial(Signals.SIGILL, timeout=0.05))
-@mark.skipif(W, reason='these tests are only for unix')
+@mark.skipif(sys.platform != 'linux', reason='these tests are only for linux')
 def test_signal_log_unix(mock_logger, wait_partial, run_in_loop):
     with raises(Log, match=r'(insufficient permissions for signal .*: .*)|(error registering signal handler: sig \d+ cannot be caught)'): run_in_loop(wait_partial(19, timeout=0.1))
     with raises(Log, match='wait_for_signal processor .* encountered expected ZeroDivisionError for signal (SIGINT|2)'):
