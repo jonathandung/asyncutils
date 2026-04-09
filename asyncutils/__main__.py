622ab5eaee9f733880b98d28db5e3efbe0a2015f
Jonathan Dung <jonathandung@yahoo.com>
2026-04-09 12:07:42 +0800

    make __main__ executable

--- asyncutils/__main__.py
+++ asyncutils/__main__.py
@@ -3,11 +3,11 @@ if __name__ != '__main__': raise ImportError('cannot import asyncutils.__main__'
 if not (g := globals()).get(k := '__package__'):
     import sys as S; n = 'asyncutils'
     if S.version_info < (3, 15): g[k] = n
-    S.path.insert(0, S.path[0].removesuffix(n)); a, b = map(S.intern, ('_frozen_importlib', 'ModuleSpec'))
+    (p := S.path).insert(0, p[0].removesuffix(n)); a, b = map(S.intern, ('_frozen_importlib', 'ModuleSpec'))
     if s := g.get('__spec__'):
         for g in type(s).__mro__:
             if g.__module__ == a and g.__qualname__ == b == g.__name__: s.name = 'asyncutils.__main__'; break
-    del a, b, s, S
+    del a, b, p, s, S
 del g, k
 from .cli import run
 raise SystemExit(run())
