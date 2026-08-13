71d8e833300a2f23abcde9d3b4219d61a53a3a0e
Jonathan Dung <jonathandung@yahoo.com>
2026-08-13 15:29:54 +0800

    how many hotfixes need i make

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,20 +4,6 @@ def setup(app, f=__import__('operator').methodcaller('replace', '|version|', rel
     app.add_directive('sub-code-block', type('SubCodeBlock', (CodeBlock,), {'run': lambda self: setattr(self, 'content', tuple(map(f, self.content))) or CodeBlock.run(self)}))
 project = 'asyncutils'
 author = 'Jonathan Dung'
-version = '1.1'
-copyright = '2026 Jonathan Dung'
-need_sphinx = '9.1.0'
-extensions = ['autoapi.extension', 'notfound.extension', 'sphinx_copybutton', 'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
-default_role = 'py:obj'
-suppress_warnings = ['autoapi.python_import_resolution']
-if __import__('os').getenv('READTHEDOCS') == 'True':
-    html_theme = 'furo'
-    html_theme_options = {'top_of_page_buttons': ['edit', 'view'], 'source_repository': f'https://github.com/jonathandung/{project}', 'source_branch': 'main', 'source_directory': 'docs/source/'}
-    source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
-    extensions += ('myst_parser', 'sphinxext.opengraph')
-    ogp_canonical_url = f'https://{project}.readthedocs.io/en/stable/'
-else: html_theme = 'sphinx_book_theme'
-html_short_title = f'{project} {release} docs'
 autoapi_dirs = [f'../../{project}']
 autoapi_file_patterns = ['*.pyi']
 autoapi_ignore = ['*/_internal/compat.pyi', '*/_internal/initialize.pyi', '*/_internal/log.pyi', '*/_internal/py312.pyi', '*/_internal/py313.pyi', '*/_internal/running_console.pyi', '*/_internal/submodules.pyi']
@@ -28,9 +14,23 @@ autoapi_python_class_content = 'both'
 autoapi_root = 'api'
 copybutton_exclude = '.linenos, .gp, .go'
 copybutton_prompt_text = '>>> '
+copyright = '2026 Jonathan Dung'
+default_role = 'py:obj'
+extensions = ['autoapi.extension', 'myst_parser', 'notfound.extension', 'sphinx_copybutton', 'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
+if __import__('os').getenv('READTHEDOCS') == 'True':
+    html_theme = 'furo'
+    html_theme_options = {'top_of_page_buttons': ['edit', 'view'], 'source_repository': f'https://github.com/jonathandung/{project}', 'source_branch': 'main', 'source_directory': 'docs/source/'}
+    source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
+    extensions.append('sphinxext.opengraph')
+    ogp_canonical_url = f'https://{project}.readthedocs.io/en/stable/'
+else: html_theme = 'sphinx_book_theme'
+html_short_title = f'{project} {release} docs'
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None), 'anyio': ('https://anyio.readthedocs.io/en/stable', None), 'more-itertools': ('https://more-itertools.readthedocs.io/en/stable', None)}
-viewcode_line_numbers = True
-maximum_signature_line_length = 120
 manpage_url = 'https://manpages.debian.org/{path}'
-nitpicky = True
+maximum_signature_line_length = 120
+need_sphinx = '9.1.0'
 nitpick_ignore_regex = [('py:(class|obj)', '[TRVUSP]|Ts'), ('py:deco', r'types\.coroutine'), ('py:class', r'_typeshed\..*|[a-z]*\.Future|TypeForm|tuple\[\]|P\.(kw)?args|tyx\..*|Abstract(Async)?ContextManager|(types\.)?(Coroutine|Frame|Function|(Async)?Generator)Type|Ellipsis|Buffer|TextIOWrapper|collections\.abc\.Callable\[\[\]'), ('py:meth', '__ae(nter|xit)__|acquire|release|wait'), ('py:class', 'ty_extensions.JustFloat')] # cSpell:disable-line
+nitpicky = True
+suppress_warnings = ['autoapi.python_import_resolution']
+version = '1.1'
+viewcode_line_numbers = True
